99fc2f277f3d16600792b569b78e1ff5c3ca95a4
Andrew Garrison <andrewgarrison@Andrews-iMac.local>
2018-09-28 22:11:07 -0500

    feat: trigger search by clicking enter key

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,21 +10,28 @@ class Search extends Component {
       };
     }
   
+    render() {
+      return (
+        <div className='container'>
+          <div className='c-search'>
+            <input type="text" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} onKeyUp={evt => this.triggerSearch(evt)} className='c-search__input' placeholder='Search for Movies'></input>
+            <Link className='c-search__submit c-btn c-primary-btn' to={`/search/${this.state.inputValue}`} replace>Find Movie</Link>
+          </div>
+        </div>
+      );
+    }
+
     updateInputValue(evt) {
       this.setState({
         inputValue: evt.target.value
       });
     }
-  
-    render() {
-      return (
-        <div className='container'>
-          <div className='c-search'>
-            <input type="text" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} className='c-search__input' placeholder='Search for Movies'></input>
-            <Link className='c-search__submit c-btn c-primary-btn' to={`/search/${this.state.inputValue}`} replace>Find Movie</Link>
-          </div>
-        </div>
-      );
+
+    triggerSearch(evt) {
+      evt.preventDefault();
+      if (evt.keyCode === 13) {
+        document.querySelector('.c-search__submit').click();
+      }
     }
   }
 
