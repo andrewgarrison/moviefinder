9c0dcf683cc30d6dbb44162075d5bfc171472bbd
Andrew Garrison <andrewgarrison@Andrews-iMac.local>
2018-10-01 16:10:58 -0500

    feat: add watch button if movie is on netflix

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cheerio from 'cheerio'
 import './MoviePage.less';
 import BackButton from './BackButton';
 import PlayIcon from '../assets/img/play-circle.svg';
@@ -9,43 +10,60 @@ class MoviePage extends Component {
       this.state = {
         movieResponse: [],
         movieBackdrop: '',
-        movieTrailer: ''
+        movieTrailer: '',
+        netflixMovies: []
       };
     }
   
     componentDidMount() {
       fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.number}?api_key=53f9a01f084ff957f8d4f94dbd002089&language=en-US`)
-      .then(results => {
-        return results.json();
-      }).then(data => {
+      .then(response => {
+        return response.json();
+      }).then(result => {
+          
           // store tmdb backdrop
-          this.setState({movieBackdrop: 'https://image.tmdb.org/t/p/original' + data.backdrop_path})
+          this.setState({movieBackdrop: 'https://image.tmdb.org/t/p/original' + result.backdrop_path})
   
           // Make a subsequent request to imdb to get improved movie data
-          fetch('https://www.omdbapi.com/?i=' + data.imdb_id + '&apikey=dbbd0a02')
-          .then(imdb_results => {
-            return imdb_results.json();
-          }).then(imdb_data => {
-            this.setState({movieResponse: imdb_data})
+          fetch('https://www.omdbapi.com/?i=' + result.imdb_id + '&apikey=dbbd0a02')
+          .then(imdb_response => {
+            return imdb_response.json();
+          }).then(imdb_result => {
+            this.setState({movieResponse: imdb_result})
           });
       })
   
       // get movie trailer link
       fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.number}/videos?api_key=53f9a01f084ff957f8d4f94dbd002089&language=en-US`)
-      .then(results => {
-        return results.json();
-      }).then(data => {
+      .then(response => {
+        return response.json();
+      }).then(result => {
         // store trailer link
-        if (data.results.length) {
-          for (let i = 0; i < data.results.length; i++) {
-            if (data.results[i].type === 'Trailer') {
-              this.setState({movieTrailer: data.results[0].key});
+        if (result.results.length) {
+          for (let i = 0; i < result.results.length; i++) {
+            if (result.results[i].type === 'Trailer') {
+              this.setState({movieTrailer: result.results[0].key});
               break;
             }
           }
         }
       });
-    }
+
+      // is it on netflix?
+      fetch('https://www.finder.com/netflix-movies')
+      .then(response => {
+        if (response.status === 200) {
+          return response.text();
+        }
+      }).then(result => {
+          let netflixList = [];
+          const $ = Cheerio.load(result);
+          $('.custom-table td b').each(function(i) {
+            netflixList[i] = $(this).text();
+          });
+          this.setState({netflixMovies: netflixList});
+      });
+    } 
   
     render() {
       return (
@@ -63,14 +81,19 @@ class MoviePage extends Component {
               <p className='c-movie__plot'>{this.state.movieResponse.Plot}</p>
               <p><strong>Starring:</strong> {this.state.movieResponse.Actors}</p>
               <a href={`https://www.imdb.com/title/${this.state.movieResponse.imdbID}`}><h3 className='c-movie__rating'>{this.state.movieResponse.imdbRating}<span className='out-of-ten'>/10</span></h3></a>
-              {this.state.movieTrailer !== '' &&
-                <a href={`https://www.youtube.com/watch?v=${this.state.movieTrailer}`} className='c-btn c-secondary-btn'>
-                  <div className='flex flex--center'>
-                    <div>Watch Trailer</div> 
-                    <div><img src={PlayIcon} alt='Play Icon' className='c-play-icon'/></div>
-                  </div>
-                </a>
-              }
+              <div className='c-btn-row'>
+                {this.onNetflix(this.state.movieResponse.Title) &&
+                  <a href={`https://www.netflix.com/search?q=${this.state.movieResponse.Title}`} className='c-btn c-primary-btn'>Watch on Netflix</a>
+                }
+                {this.state.movieTrailer !== '' &&
+                  <a href={`https://www.youtube.com/watch?v=${this.state.movieTrailer}`} className='c-btn c-secondary-btn'>
+                    <div className='flex flex--center'>
+                      <div>Watch Trailer</div> 
+                      <div><img src={PlayIcon} alt='Play Icon' className='c-play-icon'/></div>
+                    </div>
+                  </a>
+                }
+              </div>
             </div>
             <div className='c-movie__poster'>
               <img src={this.state.movieResponse.Poster} alt={this.state.movieResponse.Title}/>
@@ -78,6 +101,10 @@ class MoviePage extends Component {
           </div>
         </div>
       );
+    }
+
+    onNetflix(title) {
+      return (this.state.netflixMovies.indexOf(title) > -1);   
     }
   }
 
