bd540552a2d724387833e1989d20bfe11e32d990
Andrew_Garrison <atgarrison@unomaha.edu>
2018-10-30 17:30:08 -0500

    feat: react router page transitions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import './App.less';
 import Search from './components/Search';
 import SearchResults from './components/SearchResults';
 import MoviePage from './components/MoviePage';
+import ScrollToTop from 'react-router-scroll-top';
 
 class App extends Component {
   render() {
@@ -11,12 +13,24 @@ class App extends Component {
       <Router>
         <Route render={({ location }) => (
           <div className='movie-app'>
-            <Switch location={location}>
-              <Route exact path='/' component={Search}/>
-              <Route exact path='/search' component={Search}/>
-              <Route path='/search/:term' component={SearchResults}/>
-              <Route path='/item/:number' component={MoviePage}/>
-            </Switch>
+            <Route exact path='/' component={Search}/>
+            <Route exact path='/search' component={Search}/>
+            <TransitionGroup>
+              <CSSTransition 
+                key={location.key}
+                classNames='fade'
+                timeout={{enter: 300, exit: 300}}
+              >
+                <ScrollToTop>
+                  <div className='swtich-section'>
+                    <Switch location={location}>
+                      <Route path='/search/:term' component={SearchResults}/>
+                      <Route path='/item/:number' component={MoviePage}/>
+                    </Switch>
+                  </div>
+                </ScrollToTop>
+              </CSSTransition>
+            </TransitionGroup>
           </div>
         )}/>
       </Router>
